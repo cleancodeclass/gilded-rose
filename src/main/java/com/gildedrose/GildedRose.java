36cc3f911b7a1b2f4b3a0bf7530fef234ea8fbfb
dongsik2026-kim <dongsik2026.kim@samsung.com>
2019-10-10 12:45:02 +0900

    08-리팩토링-분기문 정리
    아이템단위의 분기문 합치기

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -19,6 +19,12 @@ class GildedRose {
 				if (item.quality < MAX_QUALITY) {
 					item.quality = item.quality + 1;
 				}
+				if (item.sellIn < 1) {
+					if (item.quality < MAX_QUALITY) {
+						item.quality = item.quality + 1;
+					}
+				}
+
 			} else if (item.name.equals(BACKSTAGE_PASSES)) {
 				if (item.quality < MAX_QUALITY) {
 					item.quality = item.quality + 1;
@@ -35,12 +41,24 @@ class GildedRose {
 						}
 					}
 				}
+				if (item.sellIn < 1) {
+					item.quality = item.quality - item.quality;
+				}
 			} else if (item.name.equals(SULFURAS)) {
 				if (item.quality > 0) {
+				}
+				if (item.sellIn < 0) {
+					if (item.quality > 0) {
+					}
 				}
 			} else {
 				if (item.quality > 0) {
 					item.quality = item.quality - 1;
+				}
+				if (item.sellIn < 1) {
+					if (item.quality > 0) {
+						item.quality = item.quality - 1;
+					}
 				}
 			}
 
@@ -49,28 +67,6 @@ class GildedRose {
 				item.sellIn = item.sellIn - 1;
 			}
 
-			if (item.name.equals(AGED_BRIE)) {
-				if (item.sellIn < 0) {
-					if (item.quality < MAX_QUALITY) {
-						item.quality = item.quality + 1;
-					}
-				}
-			} else if (item.name.equals(BACKSTAGE_PASSES)) {
-				if (item.sellIn < 0) {
-					item.quality = item.quality - item.quality;
-				}
-			} else if (item.name.equals(SULFURAS)) {
-				if (item.sellIn < 0) {
-					if (item.quality > 0) {
-					}
-				}
-			} else {
-				if (item.sellIn < 0) {
-					if (item.quality > 0) {
-						item.quality = item.quality - 1;
-					}
-				}
-			}
 		}
 	}
 }
