bf0f1642ad934e7ba3c96b8521a6c3543c665d68
dongsik2026-kim <dongsik2026.kim@samsung.com>
2019-10-10 12:23:51 +0900

    06-리팩토링-DeadCode 삭제
    실행되지 않는 코드와 중복 조건 삭제

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -18,36 +18,20 @@ class GildedRose {
 			if (item.name.equals(AGED_BRIE)) {
 				if (item.quality < MAX_QUALITY) {
 					item.quality = item.quality + 1;
-
-					if (item.name.equals(BACKSTAGE_PASSES)) {
-						if (item.sellIn < 11) {
-							if (item.quality < MAX_QUALITY) {
-								item.quality = item.quality + 1;
-							}
-						}
-
-						if (item.sellIn < 6) {
-							if (item.quality < MAX_QUALITY) {
-								item.quality = item.quality + 1;
-							}
-						}
-					}
 				}
 			} else if (item.name.equals(BACKSTAGE_PASSES)) {
 				if (item.quality < MAX_QUALITY) {
 					item.quality = item.quality + 1;
 
-					if (item.name.equals(BACKSTAGE_PASSES)) {
-						if (item.sellIn < 11) {
-							if (item.quality < MAX_QUALITY) {
-								item.quality = item.quality + 1;
-							}
+					if (item.sellIn < 11) {
+						if (item.quality < MAX_QUALITY) {
+							item.quality = item.quality + 1;
 						}
+					}
 
-						if (item.sellIn < 6) {
-							if (item.quality < MAX_QUALITY) {
-								item.quality = item.quality + 1;
-							}
+					if (item.sellIn < 6) {
+						if (item.quality < MAX_QUALITY) {
+							item.quality = item.quality + 1;
 						}
 					}
 				}
