1f6c2766bc6e6983f72921fc64101750442c034c
SDS <SDS@DESKTOP-0T0K75S>
2019-10-11 15:21:08 +0900

    GildedRose origin 브랜치 등록

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,14 @@
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
