9200ee6172bcf9a6d40f95913bcf74d0187493b0
SDS <SDS@DESKTOP-0T0K75S>
2019-10-14 15:13:16 +0900

    IF 조건문 통합

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -18,6 +18,11 @@ class GildedRose {
 				if (item.quality < MAX_QUALITY) {
 					item.quality = item.quality + 1;
 				}
+				if (item.sellIn < 1) {
+					if (item.quality < MAX_QUALITY) {
+						item.quality = item.quality + 1;
+					}
+				}
 			} else if (item.name.equals(BACKSTAGE_PASSES)) {
 				if (item.quality < MAX_QUALITY) {
 					item.quality = item.quality + 1;
@@ -34,12 +39,24 @@ class GildedRose {
 						}
 					}
 				}
+				if (item.sellIn < 1) {
+					item.quality = item.quality - item.quality;
+				}
 			} else if (item.name.equals(SULFURAS)) {
 				if (item.quality > 0) {
+				}
+				if (item.sellIn < 0) {
+					if (item.quality > 0) {
+					}
 				}
 			} else {
 				if (item.quality > 0) {
 					item.quality = item.quality - 1;
+				}
+				if (item.sellIn < 1) {
+					if (item.quality > 0) {
+						item.quality = item.quality - 1;
+					}
 				}
 			}
 
@@ -48,28 +65,6 @@ class GildedRose {
 				item.sellIn = item.sellIn - 1;
 			}
 
-			if (item.name.equals(AGED_BRIE)) {
-				if (item.sellIn < 0) {
-					if (item.quality < MAX_QUALITY) {
-						item.quality = item.quality + 1;
-					}
-				}
-			} else if (item.name.equals(BACKSTAGE_PASSES)) {
-				if (item.sellIn < 0) {
-					item.quality = item.quality - item.quality;
-				}
-			} else if (item.name.equals(SULFURAS)) {
-				if (item.sellIn < 0) {
-					if (item.quality > 0) {
-					}
-				}
-			} else {
-				if (item.sellIn < 0) {
-					if (item.quality > 0) {
-						item.quality = item.quality - 1;
-					}
-				}
-			}
 		}
 	}
 }
