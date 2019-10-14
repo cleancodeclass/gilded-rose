577a9460f4a1ab20b17c8215b73445d2ea554de5
SDS <SDS@DESKTOP-0T0K75S>
2019-10-14 14:49:11 +0900

    if 분기문 정리
    분기문을 Item의 유형에 따라 정리함

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,75 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final int MAX_QUALITY = 50;
+	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
+			Item item = items[i];
+			if (item.name.equals(AGED_BRIE)) {
+				if (item.quality < MAX_QUALITY) {
+					item.quality = item.quality + 1;
+				}
+			} else if (item.name.equals(BACKSTAGE_PASSES)) {
+				if (item.quality < MAX_QUALITY) {
+					item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+					if (item.sellIn < 11) {
+						if (item.quality < MAX_QUALITY) {
+							item.quality = item.quality + 1;
+						}
+					}
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+					if (item.sellIn < 6) {
+						if (item.quality < MAX_QUALITY) {
+							item.quality = item.quality + 1;
+						}
+					}
+				}
+			} else if (item.name.equals(SULFURAS)) {
+				if (item.quality > 0) {
+				}
+			} else {
+				if (item.quality > 0) {
+					item.quality = item.quality - 1;
+				}
+			}
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+			if (item.name.equals(SULFURAS)) {
+			} else {
+				item.sellIn = item.sellIn - 1;
+			}
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
+			if (item.name.equals(AGED_BRIE)) {
+				if (item.sellIn < 0) {
+					if (item.quality < MAX_QUALITY) {
+						item.quality = item.quality + 1;
+					}
+				}
+			} else if (item.name.equals(BACKSTAGE_PASSES)) {
+				if (item.sellIn < 0) {
+					item.quality = item.quality - item.quality;
+				}
+			} else if (item.name.equals(SULFURAS)) {
+				if (item.sellIn < 0) {
+					if (item.quality > 0) {
+					}
+				}
+			} else {
+				if (item.sellIn < 0) {
+					if (item.quality > 0) {
+						item.quality = item.quality - 1;
+					}
+				}
+			}
+		}
+	}
 }
